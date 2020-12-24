96d5e5ecdca053b5a005619845febb6593e52fe6
TedGraveson <67127389+TedGraveson@users.noreply.github.com>
2020-12-23 21:32:50 -0800

    Bug Fixing
    Pressing enter on empty squares was causing dictionary  errors

--- SudukoGUI.py
+++ SudukoGUI.py
@@ -8,6 +8,7 @@ white = (255,255,255)
 black = (0, 0, 0)
 red = (255, 0, 0)
 
+squareSize = 600/9
 
 #GUI for Suduko game
 class SudukoGUI():
@@ -28,7 +29,7 @@ class SudukoGUI():
         self.screen = pygame.display.set_mode(self.windowSize)
 
         #Current square being manipulated
-        self.selected = (0,0)
+        self.selected = None
 
         #Numbers not yet finalized
         self.sketches = dict()
@@ -44,12 +45,11 @@ class SudukoGUI():
         self.solution = deepcopy(suduko)
         SudukoSolver.solve(self.solution)
 
-        self.drawBoard() 
-        self.updateBoard()     
-               
+        self.updateScreen()               
         self.startGame()
 
     
+    #Draws Suduko board
     def drawBoard(self):
         self.screen.fill(white)
         squareSize = self.boardWidth/9
@@ -72,100 +72,99 @@ class SudukoGUI():
                 thicc = 1
             pygame.draw.line(self.screen, black, (x*squareSize, 0), (x*squareSize, 600), thicc)
         
-        font = pygame.font.SysFont("comicsans", 40)
-
+    #Draws all numbers inside squares
+    def fillSquares(self):
         for row in range(len(self.board)):
             for col in range(len(self.board[0])):
                 if(self.board[row][col] != 0):
-                    # print(str(row) + ", " + str(col))
-                    txt = font.render(str(self.board[row][col]), True, black)
-                    # self.screen.blit(txt, ((gap * row),(gap*col) ))
+                    number = self.font.render(str(self.board[row][col]), True, black)
                     #To center numbers, top left will be middle of grid square minus
                     #half of number with
-                    xPos = (squareSize*col)+ (squareSize/2 - txt.get_width()/2)
-                    yPos = (squareSize *row) + (squareSize/2 - txt.get_height()/2)
-                    self.screen.blit(txt, (xPos, yPos))
+                    xPos = (squareSize*col)+ (squareSize/2 - number.get_width()/2)
+                    yPos = (squareSize *row) + (squareSize/2 - number.get_height()/2)
+                    self.screen.blit(number, (xPos, yPos))
         
+        #Draws numbers that are not finalized
         for sketchedNum in self.sketches:
-            text = self.font.render(str(self.sketches[sketchedNum]), True, (128,128,128))
-            xPos = (self.squareSize* sketchedNum[1] )+ (self.squareSize/2 - text.get_width()/2)
-            yPos = (self.squareSize *sketchedNum[0]) + (self.squareSize/2 - text.get_height()/2)
-            self.screen.blit(text, (xPos, yPos))
+            number = self.font.render(str(self.sketches[sketchedNum]), True, (128,128,128))
+            xPos = (self.squareSize* sketchedNum[1] )+ (self.squareSize/2 - number.get_width()/2)
+            yPos = (self.squareSize *sketchedNum[0]) + (self.squareSize/2 - number.get_height()/2)
+            self.screen.blit(number, (xPos, yPos))
 
-        
-        self.drawSelected()
-        self.drawButtons()
-
-    def selectSquare(self, pos):
-        if(self.selected != pos):
-            self.selected = pos
-            
-        
+    #Draws yellow highlight over clicked square
     def drawSelected(self):
-        highlight = pygame.Surface((self.boardWidth/9 -1, self.boardHeight/9-1))
-        highlight.set_alpha(128)
-        highlight.fill((255,255,153))
-        xPos = (self.selected[1] * self.squareSize)+1
-        yPos = (self.selected[0] * self.squareSize)+1
-        self.screen.blit(highlight, (xPos, yPos))
+        if self.selected != None:
+            highlight = pygame.Surface((self.boardWidth/9 -1, self.boardHeight/9-1))
+            highlight.set_alpha(128)
+            highlight.fill((255,255,153))
+            xPos = (self.selected[1] * self.squareSize)+1
+            yPos = (self.selected[0] * self.squareSize)+1
+            self.screen.blit(highlight, (xPos, yPos))
 
+    #Draws all buttons to screen
     def drawButtons(self):
         for btn in self.buttons:
             btn.drawButton(self.screen)
 
+    #Updates clock to screen
     def drawClock(self):
         pass
 
-
-    def changeNum(self, num):
-        print("Changing array: " + str(self.selected) + " to " + str(num))
-        attempt = self.sketches.pop(self.selected)
-        #If number matches solution
-        if(attempt == self.solution[self.selected[0]][self.selected[1]]):
-            self.board[self.selected[0]][self.selected[1]] = attempt
+    #Changes square currently selected
+    def setSelect(self, pos):
+        if(self.selected != pos):
+            self.selected = pos 
         else:
-            self.errors +=1
-            print("wrong")
+            self.selected = None    
 
+    #Input for a square is tried against solution, stays if correct else error is counted and
+    #square is reset
+    def trySquare(self, num):
+        if self.sketches.get(self.selected) != None:
+            attempt = self.sketches.pop(self.selected)
+            #If number matches solution
+            if(attempt == self.solution[self.selected[0]][self.selected[1]]):
+                self.board[self.selected[0]][self.selected[1]] = attempt
+            else:
+                self.errors +=1        
 
-        
-        print(self.board)
-
-    def updateBoard(self):
+    def updateScreen(self):
         pygame.display.update()
         self.drawBoard()
-        self.selectSquare(self.selected)
+        self.fillSquares()
+        self.drawSelected()
+        self.drawButtons()
+        self.drawClock()
         
+    #User input taken before number is checked against solution    
     def sketchNum(self, num):
         row = self.selected[0]
         col = self.selected[1]
-        print("Sketch " + str(row) + str(col))
         if(self.board[row][col] == 0):
-            text = self.font.render(str(num), False, (128,128,128))
-            xPos = (self.squareSize*col)+ (self.squareSize/2 - text.get_width()/2)
-            yPos = (self.squareSize *row) + (self.squareSize/2 - text.get_height()/2)
-            self.screen.blit(text, (xPos, yPos))
+            number = self.font.render(str(num), False, (128,128,128))
+            xPos = (self.squareSize*col)+ (self.squareSize/2 - number.get_width()/2)
+            yPos = (self.squareSize *row) + (self.squareSize/2 - number.get_height()/2)
+            self.screen.blit(number, (xPos, yPos))
             self.sketches.update({(row, col) : num})
 
 
+    # def testLoop(self):
+    #     self.updateBoard()
+    #     self.updateBoard()
+    #     while self.running:
+    #         for event in pygame.event.get():
+    #             if event.type == pygame.QUIT:
+    #                 self.running = False
 
-    def testLoop(self):
-        self.updateBoard()
-        self.updateBoard()
-        while self.running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.running = False
     def startGame(self):
         while self.running :
             num = None
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
-                elif event.type == pygame.KEYDOWN:
+                elif event.type == pygame.KEYDOWN and self.selected!= None:
                     if event.key == pygame.K_RETURN:
-                        if self.sketches[self.selected]!= None:
-                            self.changeNum(self.sketches[self.selected])
+                        self.trySquare(self.sketches.get(self.selected))
                     elif event.key == pygame.K_1:
                         num = 1
                     elif event.key == pygame.K_2:
@@ -185,21 +184,22 @@ class SudukoGUI():
                     elif event.key == pygame.K_9:
                         num = 9
                     if num != None:
+
                         self.sketchNum(num)
 
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     pos = pygame.mouse.get_pos()
+                    #If mouse is on the board
                     if pos[0] <= 600:
                         row = int(pos[1] // (self.boardHeight/9))
-                        col = int(pos[0] // (self.boardWidth/9)) 
-                        print(str(row) + str(col))                       
-                        self.selectSquare((row,col))
+                        col = int(pos[0] // (self.boardWidth/9))                    
+                        self.setSelect((row,col))
                     else:
                         for btn in self.buttons:
                             if btn.mouseHover(pos):
                                 self.board = self.solution
 
-            self.updateBoard()         
+            self.updateScreen()         
             
 
 arr = [
